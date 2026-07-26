b9aecbc981d750701e71d58cfdebadf57cb336fc
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2026-07-26 22:08:36 +1000

    Add logging functions

---
+++ src/log.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void log_debug(const char *fmt, ...);
+void log_info (const char *fmt, ...);
+void log_warn (const char *fmt, ...);
+void log_error(const char *fmt, ...);
+void log_fatal(const char *fmt, ...);
